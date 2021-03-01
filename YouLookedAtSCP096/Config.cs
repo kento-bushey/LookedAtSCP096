be8510d77c334dd1ac5eef8ff846700c776ca23a
kento-bushey <43325417+kento-bushey@users.noreply.github.com>
2021-03-01 16:39:26 -0600

    Add files via upload

---
+++ YouLookedAtSCP096/Config.cs
@@ -0,0 +1,13 @@
+﻿using Exiled.API.Interfaces;
+using System.ComponentModel;
+
+namespace YouLookedAtSCP096
+{
+    public sealed class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        [Description("Sets the message broadcasted to player after looking at SCP 096.")]
+        public string LookedMessage { get; set; } = "You looked at SCP 096!";
+    }
+}
+
---
+++ YouLookedAtSCP096/Handlers/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+namespace YouLookedAtSCP096.Handlers
+{
+    class Player
+    {
+        public void OnLookedAtSCP096(AddingTargetEventArgs ev)
+        {
+            ev.Target.Broadcast(5, YouLookedAtSCP096.Instance.Config.LookedMessage);
+        }
+    }
+}
